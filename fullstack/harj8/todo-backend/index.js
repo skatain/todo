98ad72fc3333046fcdefeadb16c7d020b95d0268
Samuli Katainen <ad6906@student.jamk.fi>
2023-12-02 22:17:37 +0200

    add todo-backend

--- fullstack/harj8/todo-backend/index.js
+++ fullstack/harj8/todo-backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express') 
 const cors = require('cors')
 const app = express()
-// const port = 3000
+const port = 3000
 
 // cors - allow connection from different domains and ports
 app.use(cors())
@@ -63,7 +63,7 @@ app.get('/todos', async (request, response) => {
 // app.listen(port, () => {
 //   console.log('Example app listening on port 3000')
 // })
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
