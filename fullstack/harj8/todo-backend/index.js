adc216dfd754acada875acbb0852754ed8df9fbc
skatain <152724168+skatain@users.noreply.github.com>
2023-12-03 16:34:21 +0200

    Update index.js

--- fullstack/harj8/todo-backend/index.js
+++ fullstack/harj8/todo-backend/index.js
@@ -63,7 +63,7 @@ app.get('/todos', async (request, response) => {
 // app.listen(port, () => {
 //   console.log('Example app listening on port 3000')
 // })
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
