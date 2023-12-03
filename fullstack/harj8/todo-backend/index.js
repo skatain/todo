b2d36cdca0dfd073c991aaa78b4ca56b0a214960
skatain <152724168+skatain@users.noreply.github.com>
2023-12-03 19:17:37 +0200

    Update index.js

--- fullstack/harj8/todo-backend/index.js
+++ fullstack/harj8/todo-backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express') 
 const cors = require('cors')
 const app = express()
-const port = 3001
+const port = 3000
 
 // cors - allow connection from different domains and ports
 app.use(cors())
@@ -59,11 +59,11 @@ app.get('/todos', async (request, response) => {
     else response.status(404).end()
   })
 
-// app listen port 3000
-// app.listen(port, () => {
-//   console.log('Example app listening on port 3000')
-// })
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app listen port 3000
+app.listen(port, () => {
+  console.log('Example app listening on port 3000')
 })
+// const PORT = process.env.PORT || 3001
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
